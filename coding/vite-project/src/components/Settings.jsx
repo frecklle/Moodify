958504948afa1b5e6a06e1180c9f0b3b1a9e2907
Anastasiia Vudvud <anastasiiavudvud@gmail.com>
2024-11-25 19:15:19 +0100

    error handling window

--- coding/vite-project/src/components/Settings.jsx
+++ coding/vite-project/src/components/Settings.jsx
@@ -31,11 +31,30 @@ const Settings = () => {
             window.location.href = "http://localhost:5001/spotify/auth";
         }
     };
+    const params = new URLSearchParams(window.location.search);
+    const error = params.get('error');
+    
 
     return (
         <div className="flex justify-center items-center w-[100vw] min-h-screen bg-[#EDF1D6]">
-            <div className="w-full max-w-sm bg-white p-8 border border-gray-300 rounded-lg shadow-lg">
+            <div className="w-full max-w-sm bg-white p-8 border border-gray-300 rounded-lg shadow-lg text-center">
                 <h2 className="text-2xl font-bold text-black mb-6">Settings</h2>
+                
+                {error && (
+                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                        <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+                            <h3 className="text-xl font-bold text-black-500 mb-4">Error: {error}</h3>
+                            <p className="mb-4">Please try again</p>
+                            <button
+                                onClick={() => window.location.search = ''}
+                                className="bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-400 transition duration-300 ease-in-out"
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                )}
+                
                 <div className="flex flex-col space-y-4">
                     <button
                         onClick={handleChangeEmail}
